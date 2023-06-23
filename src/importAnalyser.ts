382cb93dd62efcfdf39eb63857c2945181dc0680
ibragimov.i <Tointer@yandex.ru>
2023-06-24 00:48:03 +0800

    add import analyser script

---
+++ src/importAnalyser.ts
@@ -0,0 +1,28 @@
+import mainnet from './mainnetContext.json' assert { type: "json" };
+
+interface ImportCheckResult{
+    wrong_contracts: string[];
+}
+
+function checkImports(transaction : string){
+    const result : ImportCheckResult = {
+        wrong_contracts: []
+    };
+
+    const importRegex = /import\s+([a-zA-Z0-9]+)\s+from\s+(0x[a-zA-Z0-9]+)/g;
+    const matched = transaction.matchAll(importRegex);
+    const arrays = Array.from(matched).map(x => [x[1], x[2]]);
+
+    const mainnetDict = Object.entries(mainnet);
+
+    for (const [contractName, contractAddress] of arrays) {
+        if(mainnetDict.find(x => x[0] === contractName && x[1] !== contractAddress) !== undefined){
+            result.wrong_contracts.push(contractName);
+        }
+    }
+
+    console.log(result);
+
+}
+
+export default {checkImports};
