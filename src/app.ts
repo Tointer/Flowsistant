51905b2dd283b33245cd96090bc8d58e2d722ca9
ibragimov.i <Tointer@yandex.ru>
2023-06-27 00:36:00 +0800

    create server in entry point

--- src/app.ts
+++ src/app.ts
@@ -2,6 +2,8 @@ import * as dotenv from 'dotenv';
 import * as fcl from "@onflow/fcl";
 import txAnalyser from './txAnalyser.js';
 import txForge from './txForge.js';
+import express from 'express';
+import path from 'path';
 
 fcl.config({
   "accessNode.api": "https://rest-mainnet.onflow.org",
@@ -10,22 +12,28 @@ fcl.config({
 
 dotenv.config();
 
+const app = express();
+
+//frontend
+app.use(express.static(`${path.resolve()}/frontend-build`));
+
+//api
+app.get("/api", (req, res) => {
+  return res.json({ message: "this is api path" })
+});
+
+// other path
+app.use("/", (req, res) => {
+  return res.sendFile(path.join(__dirname + "/frontend-build/index.html"))
+});
+
+app.listen("3333", () => {
+  console.log("running on 3333")
+})
+
 //const tx = txForge.fiatTransfer(mainnet.FungibleToken, mainnet.FiatToken, 2000, '0xdd765a6bf207c051')
-const tx = txForge.buyTopshotMoment(`0xf919ee77447b7497`, '0xdd765a6bf207c051')
+// const tx = txForge.buyTopshotMoment(`0xf919ee77447b7497`, '0xdd765a6bf207c051')
 
-const result = await txAnalyser.txAnalyse(tx);
-console.log(result);
-
-
-// await fcl.query({
-//   cadence: `
-//     import Profile from 0x0b2a3299cc857e29
-
-//     pub fun main(address: Address): Profile.ReadOnly? {
-//       return Profile.read(address)
-//     }
-//   `,
-//   args: (arg, t) => [arg("0x0b2a3299cc857e29", t.Address)]
-// });
-
+// const result = await txAnalyser.txAnalyse(tx);
+// console.log(result);
 
