0d64ef79647e9276888da9352922f36ff298abef
ibragimov.i <Tointer@yandex.ru>
2023-06-29 23:30:20 +0800

    add dark mode by default, remove starter code

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,34 +1,16 @@
 import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import AskForm from './AskForm.tsx'
+import {CssVarsProvider} from '@mui/joy/styles';
+ 
 
 function App() {
-  const [count, setCount] = useState(0)
-
   return (
-    <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
+    <CssVarsProvider defaultMode='dark'>
+      <h1 className="text-3xl font-bold">Transaction analyzer</h1>
+      <div className="content-center text-center">
+        <AskForm></AskForm>
       </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
+    </CssVarsProvider>
   )
 }
 
