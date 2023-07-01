23e5ab2f72a42c943adb8bacee4135dab878c5b7
ibragimov.i <Tointer@yandex.ru>
2023-07-01 23:50:58 +0800

    add NavBar mock

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -2,16 +2,20 @@ import { useState } from 'react'
 import AskForm from './AskForm.tsx'
 import {CssVarsProvider} from '@mui/joy/styles';
 import './App.css'
+import NavBar from './NavBar.tsx'
  
 
 function App() {
   return (
-    <CssVarsProvider defaultMode='dark'>
-      <h1 className="text-3xl font-bold">Transaction analyzer</h1>
-      <div className="content-center text-center">
+    <>
+    <NavBar tabs={["AMA", "TX analyser"]} onTabChange={() => {}}></NavBar>
+    <div className='mt-10 flex flex-col text-center'>
+      <CssVarsProvider defaultMode='dark'>
+        <h1 className="text-3xl font-bold">Transaction analyzer</h1>
         <AskForm></AskForm>
-      </div>
-    </CssVarsProvider>
+      </CssVarsProvider>
+    </div>
+    </>
   )
 }
 
---
+++ frontend/src/NavBar.tsx
@@ -0,0 +1,77 @@
+import * as React from 'react';
+import Box from '@mui/joy/Box';
+import Chip from '@mui/joy/Chip';
+import Tabs from '@mui/joy/Tabs';
+import TabList from '@mui/joy/TabList';
+import Tab, { tabClasses } from '@mui/joy/Tab';
+import TabPanel from '@mui/joy/TabPanel';
+import Typography from '@mui/joy/Typography';
+
+export default function NavBar(props: {tabs: string[], onTabChange: (tabIndex: number) => void}) {
+  const [index, setIndex] = React.useState(0);
+  return (
+    <Box
+      sx={{
+        bgcolor: 'background.body',
+        flexGrow: 1,
+        m: -3,
+        overflowX: 'hidden',
+        borderRadius: 'md',
+      }}
+    >
+      <Tabs
+        aria-label="Pipeline"
+        value={index}
+        onChange={(event, value) => setIndex(value as number)}
+        sx={{ '--Tabs-gap': '0px' }}
+      >
+        <TabList
+          variant="plain"
+          sx={{
+            width: '100%',
+            maxWidth: 400,
+            mx: 'auto',
+            pt: 2,
+            alignSelf: 'flex-start',
+            [`& .${tabClasses.root}`]: {
+              bgcolor: 'transparent',
+              boxShadow: 'none',
+              '&:hover': {
+                bgcolor: 'transparent',
+              },
+              [`&.${tabClasses.selected}`]: {
+                color: 'primary.plainColor',
+                fontWeight: 'lg',
+                '&:before': {
+                  content: '""',
+                  display: 'block',
+                  position: 'absolute',
+                  zIndex: 1,
+                  bottom: '-1px',
+                  left: 'var(--ListItem-paddingLeft)',
+                  right: 'var(--ListItem-paddingRight)',
+                  height: '3px',
+                  borderTopLeftRadius: '3px',
+                  borderTopRightRadius: '3px',
+                  bgcolor: 'primary.500',
+                },
+              },
+            },
+          }}
+        >
+        {props.tabs.map((tab, index) => (<Tab>{tab}</Tab>))}
+        </TabList>
+        <Box
+          sx={(theme) => ({
+            '--bg': theme.vars.palette.background.level3,
+            height: '1px',
+            background: 'var(--bg)',
+            boxShadow: '0 0 0 100vmax var(--bg)',
+            clipPath: 'inset(0 -100vmax)',
+          })}
+        />
+        
+      </Tabs>
+    </Box>
+  );
+}
