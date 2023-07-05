318fe5e7cf44b1fec9eef634d54229e36f449b58
ibragimov.i <Tointer@yandex.ru>
2023-07-06 03:14:02 +0800

    fix

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,7 +1,6 @@
 import './globals.css'
 import NavBar from './NavBar';
 import { Public_Sans } from 'next/font/google'
-import { CssBaseline } from '@nextui-org/react';
 
 const publicSans = Public_Sans({ subsets: ['latin'] })
 
