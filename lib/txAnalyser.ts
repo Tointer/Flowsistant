32a5ed872eba7ccd193a9407db20015d08bf1df1
ibragimov.i <Tointer@yandex.ru>
2023-07-06 03:14:42 +0800

    add scam check to tx analyser

--- lib/txAnalyser.ts
+++ lib/txAnalyser.ts
@@ -1,22 +1,28 @@
 import txForge from './txForge';
 import importAnalyser from './importAnalyser';
+import scamChecker from './scamChecker';
 import promter from './promter';
 import { ResponseCategory } from './types';
 
-enum ResponseOverride {
-    None,
-    WrongImports    = 1 << 1,
-}
+// enum ResponseOverride {
+//     None,
+//     WrongImports    = 1 << 1,
+//     Scam            = 1 << 2,
+// }
 
 export async function txAnalyse(tx : string): Promise<{answer: string, cat: ResponseCategory}>{
 
-    let overrides =  ResponseOverride.None;
     let cat = ResponseCategory.none;
     let answer = "";
 
     const wrongInputs = importAnalyser.checkImports(tx);
-    if(wrongInputs.wrong_contracts.length > 0){
-        overrides |= ResponseOverride.WrongImports;
+    const scamAddresses = scamChecker.checkScam(tx);
+
+    if(scamAddresses.length > 0){
+        cat = ResponseCategory.alarm;
+        answer = `Your transaction is interacting with scam address ${scamAddresses[0]}, do NOT proceed!`;
+    }
+    else if(wrongInputs.wrong_contracts.length > 0){
         cat = ResponseCategory.warning
         answer = `Your transaction are interacting with contract that have name of some popular contract, but with different address.
         This can be a sign of scam, or some new version of contract, please double check before proceeding`
