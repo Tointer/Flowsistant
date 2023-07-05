ced6d59cbd0af7894247f5455784a7bd2fb38c62
ibragimov.i <Tointer@yandex.ru>
2023-07-06 03:14:26 +0800

    refactor scam checker

--- lib/scamChecker.ts
+++ lib/scamChecker.ts
@@ -1,12 +1,13 @@
 import mainnet from './mainnetContext.json' assert { type: "json" };
 
-function checkScamInteraction(transaction : string): string[]{
+function checkScam(tx : string): string[]{
     
     //regex that matches all addresses in transaction, starting with 0x and spanning 18 characters
-    const addressRegex = /0x[a-zA-Z0-9]{18}/g;
-    const matched = transaction.matchAll(addressRegex);
+    const addressRegex = /0x[a-zA-Z0-9]{16}/g;
+    const matched = Array.from(tx.matchAll(addressRegex)).map(x => x[0]);
+    console.log("Addresses: " + matched)
 
-    return Array.from(matched).map(x => x[0]).filter(x => Object.entries(mainnet.scam).includes(x as any));
+    return matched.filter(x => mainnet.scam.includes(x));
 }
 
-export default {checkScamInteraction};
+export default {checkScam};
