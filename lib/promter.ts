8e731a1703184941193dac57962c25ec637c25e4
ibragimov.i <Tointer@yandex.ru>
2023-07-09 22:30:25 +0800

    add promt for linking

--- lib/promter.ts
+++ lib/promter.ts
@@ -44,4 +44,49 @@ async function promtTxAnalyse(transaction : string, userContext: UserContext){
 
 }
 
-export default {promtTxAnalyse};
+async function promtLinkingAccounts(transaction : string): Promise<{parent: string, child: string}>{
+    
+    if (process.env.OPENAI_API_KEY === undefined) {
+        throw new Error('OPENAI_API_KEY is not defined')
+    }
+    
+    const encoder = getEncoding("gpt2");
+    
+    const api = new ChatGPTAPI({
+        apiKey: process.env.OPENAI_API_KEY as string,
+    })
+
+
+    const rulesBlock = `
+    You are digital advisor for Flow blockchain. You will be given transactions that includes acount linking. 
+    Account linking is onboarding process in which user gettting custody over his NFTs in child account. Typically, child account is created by some game or app that user is using.
+    Your task is to understand relationship between parent and child accounts in this transaction.
+    You should always giving your best shot and never answering that you can't determine if transaction is good or bad. Work with informantion given.
+    Your answer should be in json format "{parent: parent address, child: child address}"
+    `
+
+    const transactionBlock = `
+    Here is the transaction, written in Cadence language: ${transaction}}`
+
+    const finalInput = rulesBlock + transactionBlock;
+
+    console.log(`token count: ${encoder.encode(finalInput).length}`)
+
+    const answer = (await api.sendMessage(finalInput)).text
+    //const result = "Mock GPT-3 response";
+
+    let result : {parent: string, child: string};
+    try {
+        result = JSON.parse(answer.trim());
+    }
+    catch (e) {
+        console.log(e);
+        result = {parent: "", child: ""};
+    }
+    return result;
+
+}
+
+
+
+export default {promtTxAnalyse, promtLinkingAccounts};
