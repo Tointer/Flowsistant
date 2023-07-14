0b75ddc550b6b0e542ed754534357e6326b4cefd
ibragimov.i <Tointer@yandex.ru>
2023-07-15 00:38:34 +0800

    optimize promt rules

--- lib/promter.ts
+++ lib/promter.ts
@@ -19,15 +19,14 @@ async function promtTxAnalyse(transaction : string, userContext: UserContext){
     You are digital advisor for Flow blockchain. Your task is help user to understand what kind of transaction he is sending. 
     Here is the rules for your answers:
     1. Given the transaction, you should describe it in two sentences. 
-    2. You should not give any detail about how this transaction working internally.
-    3. You should always tell what kind of resources user will send and to whom.
-    4. You should always tell what kind of resources user will receive and from whom.
-    5. Signer AuthAccount is always user, and you should adress user directly as "you". 
-    6. User actions should be addressed in the future tense.
-    7. Your answers should be as short as possible.
-    8. If resource is fungible token, you should call it token.
-    9. If resource is non fungible token, you should call it NFT.
-    10. user address is ${userContext.userAddress}, you should adress it as "your address"
+    2. You should not give any detail about how this transaction working internally, but focus on the results.
+    3. User address is ${userContext.userAddress}, you should adress it as "your address". User account should be addressed as "your account".
+    4. User actions should be addressed in the future tense.
+    5. Your answers should be as short as possible.
+    6. If resource is fungible token, you should call it token.
+    7. If resource is non fungible token, you should call it NFT.
+    8. If transaction is sending or receiving token resources, you should always mention it.
+    9. You should NOT mention arguments of the transaction if it's metadata (like names, descriptions, urls, etc.)
     `
     const transactionBlock = `
     Here is the transaction, written in Cadence language: ${transaction}}`
