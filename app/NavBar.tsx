206083b9a2179e8e873a58ec8bd3a95cbfd2e797
ibragimov.i <Tointer@yandex.ru>
2023-07-17 05:53:09 +0800

    switch tx-ask and ama-ask in navbar

--- app/NavBar.tsx
+++ app/NavBar.tsx
@@ -18,10 +18,10 @@ export default function NavBar() {
             <a href="/" className="block py-2 pl-3 pr-4 text-white bg-blue-700 rounded md:bg-transparent md:p-0  md:dark:bg-transparent" aria-current="page">Home</a>
           </li>
           <li>
-            <a href="/interface/ama" className="block py-2 pl-3 pr-4 text-white rounded hover:bg-gray-100 md:hover:bg-transparent md:border-0 md:hover:text-blue-700 md:p-0 dark:text-white md:dark:hover:text-blue-500 dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent">Ask anything</a>
+            <a href="/interface/tx-ask" className="block py-2 pl-3 pr-4 text-white rounded hover:bg-gray-100 md:hover:bg-transparent md:border-0 md:hover:text-blue-700 md:p-0 dark:text-white md:dark:hover:text-blue-500 dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent">TX analyser</a>
           </li>
           <li>
-            <a href="/interface/tx-ask" className="block py-2 pl-3 pr-4 text-white rounded hover:bg-gray-100 md:hover:bg-transparent md:border-0 md:hover:text-blue-700 md:p-0 dark:text-white md:dark:hover:text-blue-500 dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent">TX analyser</a>
+            <a href="/interface/ama" className="block py-2 pl-3 pr-4 text-white rounded hover:bg-gray-100 md:hover:bg-transparent md:border-0 md:hover:text-blue-700 md:p-0 dark:text-white md:dark:hover:text-blue-500 dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent">Ask anything</a>
           </li>
         </ul>
       </div>
